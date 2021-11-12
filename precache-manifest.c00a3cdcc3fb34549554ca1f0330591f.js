29d29a9d857d4b093251fa6fdd27131f20e0f9f2
sshanmugagani <sshanmugagani@acadian-asset.com>
2021-11-11 23:41:24 -0500

    Updates

---
+++ precache-manifest.c00a3cdcc3fb34549554ca1f0330591f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d84b46f25aa0a84dc922",
+    "url": "/me/static/css/main.d6afcbfc.chunk.css"
+  },
+  {
+    "revision": "d84b46f25aa0a84dc922",
+    "url": "/me/static/js/main.d84b46f2.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "0f79782587a3c5672f4c62012854367e",
+    "url": "/me/index.html"
+  }
+];
